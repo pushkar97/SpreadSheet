bc6ab1295fd62c6a6f25b2bca95eb29339228e08
Pushkar Desai <32242831+pushkar97@users.noreply.github.com>
2019-11-26 17:49:01 +0530

    Added hyperlink support

--- SpreadSheet.java
+++ SpreadSheet.java
@@ -14,13 +14,16 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.poi.common.usermodel.HyperlinkType;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.BorderStyle;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.CreationHelper;
 import org.apache.poi.ss.usermodel.DateUtil;
 import org.apache.poi.ss.usermodel.FillPatternType;
 import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.Hyperlink;
 import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Row.MissingCellPolicy;
@@ -54,8 +57,9 @@ public class SpreadSheet {
 	private String fileExtensionName;
 	private int rowid;
 	private int cellid;
-
-	
+	private CreationHelper createHelper;
+	private CellStyle hyperlinkStyle;
+	private Font hyperlinkFont;
 	public int getRowid() {
 		return rowid;
 	}
@@ -82,6 +86,7 @@ public class SpreadSheet {
 		}
 		this.file = file;
 		createWorkbook();
+		setDefaults();
 	}
 	
 	private void createWorkbook() throws IOException {
@@ -111,10 +116,18 @@ public class SpreadSheet {
 				throw new IOException("Invalid file type");
 			}
 		}
-		
 		System.out.println("File successfully loaded : "+file.getAbsolutePath());
 	}
-
+	
+	private void setDefaults() {
+		createHelper = workbook.getCreationHelper();
+		hyperlinkStyle = workbook.createCellStyle();
+		hyperlinkFont = workbook.createFont();
+		hyperlinkFont.setUnderline(Font.U_SINGLE);
+		hyperlinkFont.setColor(IndexedColors.BLUE.getIndex());
+		hyperlinkStyle.setFont(hyperlinkFont);
+	}
+	
 	/**
 	 * @param sheetname - Sheet name to which data will be written. if sheet is present data will be overwritten, if not new sheet will be created
 	 * @param initRowId - row number from which data will be written, useful while making multiple write calls to same sheet.
@@ -156,6 +169,19 @@ public class SpreadSheet {
 		style = workbook.createCellStyle();
 		font = workbook.createFont();
 		for (String tag : tagMatches) {
+			if (tag.toUpperCase().matches("\\<POI-HYPERLINK.*\\>")) {
+				Map<String,String> attrs = getAttributes(tag.toUpperCase());
+				System.out.println(attrs.keySet().toString());
+				if(attrs.containsKey("TYPE") && attrs.containsKey("URL")) {
+					try {
+						Hyperlink link = createHelper.createHyperlink(HyperlinkType.valueOf(attrs.get("TYPE")));
+			            link.setAddress(attrs.get("URL"));
+			            cell.setCellStyle(hyperlinkStyle);
+			            cell.setHyperlink(link);
+			    		return cellValue.replaceAll(tagRegex, "");
+					}catch(IllegalArgumentException e) {}
+				}
+			}
 			if (tag.toUpperCase().equals("<POI-BOLD/>")) {
 				font.setBold(true);
 			}
@@ -239,8 +265,8 @@ public class SpreadSheet {
 		return cellValue.replaceAll(tagRegex, "");
 	}
 	
-	Map<String,String> getAttributes(String tag){
-		String attrRegex = "\\w+\\s*=\\s*\"\\w+\"";
+	private Map<String,String> getAttributes(String tag){
+		String attrRegex = "\\w+\\s*=\\s*\".+?\"";
 		Pattern attrPattern = Pattern.compile(attrRegex);
 		Matcher tagMatcher = attrPattern.matcher(tag);
 		Map<String,String> attrMatches = new HashMap<String,String>();
@@ -382,16 +408,23 @@ public class SpreadSheet {
 		row2.add("h3<POI-BGCOLOR = \"Gold\"/>");
 		List<Object> row3 = new ArrayList<Object>();
 		List<Object> row4 = new ArrayList<Object>();
-		row4.add("true<POI-BORDER ALL=\"THICK\" COLOR=\"RED\"/>");
-		row4.add("256<POI-FONTCOLOR=\"RED\"/>");
+		row4.add("true<POI-BORDER ALL=\"MEDIUM_DASHED\" COLOR=\"TEAL\"/>");
+		row4.add("256<POI-FONTCOLOR=\"Green\"/><POI-BGCOLOR=\"LIGHT_Green\"/>");
 		row4.add("1997-11-25<POI-BOLD/>");
 		row4.add(new SimpleDateFormat("yyyy-MM-dd").parse("1997-05-17"));
+		
+		List<Object> row5 = new ArrayList<Object>();
+		row5.add("true<POI-BORDER ALL=\"MEDIUM_DASHED\" COLOR=\"TEAL\"/>");
+		row5.add("pushkar97<POI-HYPERLINK TYPE=\"URL\" URL=\"https://github.com/pushkar97\" />");
+		row5.add("1997-11-25<POI-BOLD/>");
+		row5.add(new SimpleDateFormat("yyyy-MM-dd").parse("1997-05-17"));
 		data.add(row1);
 		data.add(null);
 		data.add(row2);
 		data.add(row3);
 		data.add(row4);
 		data.add(row4);
+		data.add(row5);
 		sheet.writeData("dataWriteTest", 0, 0, data);
 		sheet.saveWorkbook();
 		sheet.closeWorkbook();
